5ca71befcfbab439fde1f6faa712573063341a19
Jacob Raihle kdm951 <jacob.raihle@teliacompany.com>
2023-12-02 08:03:52 +0200

    Day 1 cleanup

--- days/1/index.js
+++ days/1/index.js
@@ -1,7 +1,28 @@
-const FIRST_DIGIT = /^\D*(\d)/;
-const LAST_DIGIT = /(\d)\D*$/;
+import { sum } from "../../utils.js";
+
+export async function run(input) {
+  const lines = input.trim().split("\n");
+  console.log("A:", a(lines));
+  console.log("B:", b(lines));
+}
+
+function a(lines) {
+  return lines.map(firstAndLastDigit).reduce(sum);
+}
+
+function b(lines) {
+  return lines.map(replaceDigitWords).map(firstAndLastDigit).reduce(sum);
+}
+
+function firstAndLastDigit(text) {
+  const digits = text.match(/\d/g);
+  const first = digits[0];
+  const last = digits.at(-1);
+  return Number(`${first}${last}`);
+}
 
 function replaceDigitWords(text) {
+  // These replacements could be much shorter, but I'm keeping it simple
   return text
     .replace(/one/g, "one1one")
     .replace(/two/g, "two2two")
@@ -12,43 +33,4 @@ function replaceDigitWords(text) {
     .replace(/seven/g, "seven7seven")
     .replace(/eight/g, "eight8eight")
     .replace(/nine/g, "nine9nine");
-}
-
-function extractNumberA(text) {
-  const first = text.match(FIRST_DIGIT)[1];
-  const last = text.match(LAST_DIGIT)[1];
-  return Number(`${first}${last}`);
-}
-
-function extractNumberB(text) {
-  return extractNumberA(replaceDigitWords(text));
-}
-function sum(numbers) {
-  return numbers.reduce((acc, next) => acc + next);
-}
-
-function a(input) {
-  return sum(input.trim().split("\n").map(extractNumberA));
-}
-
-function b(input) {
-  return sum(input.trim().split("\n").map(extractNumberB));
-}
-
-const testInput = `1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet`;
-
-const testInput2 = `two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen`;
-
-export async function run(input) {
-  console.log("A:", a(input));
-  console.log("B:", b(input));
 }
