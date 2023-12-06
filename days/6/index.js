94b1e95eb71a62596a138212fe1dd8cf108bd52a
Jacob Raihle kdm951 <jacob.raihle@teliacompany.com>
2023-12-06 07:53:21 +0200

    Day 6 cleanup

--- days/6/index.js
+++ days/6/index.js
@@ -1,27 +1,38 @@
 import { product } from "../../utils.js";
 
 export function run(input) {
-  console.log("A:", a(parseRaces(input.trim())));
+  console.log("A:", solve(parseRaces(input.trim())));
+  console.log("B:", solve(parseRaces(input.replace(/ +/g, ""))));
 }
 
-function a(races) {
+function solve(races) {
   const waysToWin = races.map(waysToWinRace);
   return waysToWin.reduce(product);
 }
 
-function waysToWinRace({ time, distance }) {
+function waysToWinRace(race) {
   // Waiting 1 ms increases our speed by 1 mm / ms
   // The optimal time is achieved by waiting half the time
-  // Our time to win is symmetrical around this
+  // Our time to win is symmetrical around this (because distance is just speed * time)
   // But usually we do not need to be optimal to win
+  // There are three kinds of outcomes:
+  // - Not waiting enough (speed too low)
+  // - Winning
+  // - Waiting too much (time too low)
+  // This means we have a single inflection point (in "the middle")
+  // With these characteristics we only need to find "first" win on either side and can
+  // calculate how many values are inside the winning range based on the distance to the
+  // middle.
 
-  let ways = 0;
-  for (let waited = 1; waited <= time - 1; waited++) {
-    if (waited * (time - waited) > distance) {
-      ways++;
+  for (let waited = 1; true; waited++) {
+    if (isWinning(waited, race)) {
+      return race.time - waited * 2 + 1;
     }
   }
-  return ways;
+}
+
+function isWinning(waitTime, race) {
+  return waitTime * (race.time - waitTime) > race.distance;
 }
 
 function parseRaces(input) {
@@ -35,7 +46,6 @@ function parseRaces(input) {
       distance: distances[i],
     });
   }
-  console.log(races);
   return races;
 }
 
