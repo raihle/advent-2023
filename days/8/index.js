1680e7e265ddb2db21dc8173bd40593a604c5a7a
Jacob Raihle kdm951 <jacob.raihle@teliacompany.com>
2023-12-08 08:14:53 +0200

    Day 8 part 2

--- days/8/index.js
+++ days/8/index.js
@@ -1,10 +1,12 @@
-import { product } from "../../utils.js";
+import { min } from "../../utils.js";
 
 export function run(input) {
-  console.log("A:", a(parseMap(input.trim())));
+  const map = parseMap(input.trim());
+  console.log("A:", a(map));
+  console.log("B:", b(map));
 }
 
-function a({nodes, instructions}) {
+function a({ nodes, instructions }) {
   const start = "AAA";
   const goal = "ZZZ";
   let location = start;
@@ -15,6 +17,63 @@ function a({nodes, instructions}) {
     steps++;
   }
   return steps;
+}
+
+function b({ nodes, instructions }) {
+  let ghosts = Object.keys(nodes)
+    .filter((node) => node.endsWith("A"))
+    .map((node) => {
+      return {
+        node,
+        seenZ: {},
+        inCycle: false,
+        cycleStart: 0,
+        cycleLength: 0,
+      };
+    });
+  let steps = 0;
+  while (ghosts.some(({ inCycle }) => !inCycle)) {
+    const nextStep = instructions[steps % instructions.length];
+    steps++;
+    ghosts.forEach((ghost) => {
+      const nextNode = nodes[ghost.node][nextStep];
+      ghost.node = nextNode;
+      if (!ghost.inCycle && nextNode.endsWith("Z")) {
+        if (ghost.seenZ[nextNode]) {
+          ghost.inCycle = true;
+          ghost.seenZ[nextNode].push(steps);
+          ghost.cycleLength = steps - ghost.cycleStart;
+        } else {
+          ghost.seenZ[nextNode] = [steps];
+          ghost.cycleStart = steps;
+        }
+      }
+    });
+  }
+
+  console.warn("Found cycles");
+
+  const cycles = ghosts.map((ghost) => {
+    return {
+      start: ghost.cycleStart,
+      length: ghost.cycleLength,
+    };
+  });
+  const firstCycleStart = min(cycles.map((cycle) => cycle.start));
+  const smallestCycleLength = min(cycles.map((cycle) => cycle.length));
+  let at = firstCycleStart;
+  let rounds = 0;
+  while (cycles.some((cycle) => (at - cycle.start) % cycle.length != 0)) {
+    at += smallestCycleLength;
+    rounds++;
+    if (rounds % 100000000 == 0) {
+      console.warn(`Round ${rounds} testing ${at}`);
+    }
+    if (at >= Number.MAX_SAFE_INTEGER) {
+      throw new Error("Reached MAX_SAFE_INTEGER");
+    }
+  }
+  return at;
 }
 
 function parseMap(input) {
