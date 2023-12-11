b923e82b5d614ebc83920186b04ba2a6f99a322a
Jacob Raihle kdm951 <jacob.raihle@teliacompany.com>
2023-12-11 07:36:37 +0200

    Day 11 part 2

--- days/11/index.js
+++ days/11/index.js
@@ -2,18 +2,37 @@ import { sum } from "../../utils.js";
 
 export function run(input) {
   const galaxyMap = parseUniverse(input.trim());
-  console.log("A:", a(galaxyMap));
+  const emptySpace = findEmptySpace(galaxyMap);
+  const galaxies = findGalaxies(galaxyMap);
+  const pairs = makePairs(galaxies);
+  console.log("A:", a(pairs, emptySpace));
+  console.log("B:", b(pairs, emptySpace));
 }
 
-function a(galaxyMap) {
-  const expanded = expandEmptySpace(galaxyMap);
-  const galaxies = findGalaxies(expanded);
-  const pairs = makePairs(galaxies);
-  const distances = pairs.map(
-    ([a, b]) => Math.abs(a.y - b.y) + Math.abs(a.x - b.x)
-  );
-  console.log(expanded, galaxies, pairs, distances);
+function a(pairs, emptySpace) {
+  const distances = pairs.map(([a, b]) => galaxyDistance(a, b, emptySpace, 2));
   return distances.reduce(sum);
+}
+
+function b(pairs, emptySpace) {
+  const distances = pairs.map(([a, b]) =>
+    galaxyDistance(a, b, emptySpace, 1000000)
+  );
+  return distances.reduce(sum);
+}
+
+function galaxyDistance(a, b, emptySpace, expansionFactor) {
+  const unexpandedDistance = Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+  const passedEmptyRows = emptySpace.rows.filter(
+    (row) => row > Math.min(a.y, b.y) && row < Math.max(a.y, b.y)
+  ).length;
+  const passedEmptyCols = emptySpace.cols.filter(
+    (col) => col > Math.min(a.x, b.x) && col < Math.max(a.x, b.x)
+  ).length;
+  return (
+    unexpandedDistance +
+    (expansionFactor - 1) * (passedEmptyRows + passedEmptyCols)
+  );
 }
 
 function findGalaxies(galaxyMap) {
@@ -38,27 +57,25 @@ function makePairs(items) {
   return pairs;
 }
 
-function expandEmptySpace(galaxyMap) {
-  let expandedMap = [];
-  galaxyMap.forEach((line) => {
+function findEmptySpace(galaxyMap) {
+  const emptyRows = [];
+  const emptyCols = [];
+  galaxyMap.forEach((line, y) => {
     if (!line.includes("#")) {
-      expandedMap.push(line);
+      emptyRows.push(y);
     }
-    expandedMap.push(line);
   });
-  let addedCols = 0;
   for (let x = 0; x < galaxyMap[0].length; x++) {
     const column = galaxyMap.map((row) => row[x]);
     if (!column.includes("#")) {
-      expandedMap = expandedMap.map(
-        (row) =>
-          row.substring(0, x + addedCols) + "." + row.substring(x + addedCols)
-      );
-      addedCols++;
+      emptyCols.push(x);
     }
   }
 
-  return expandedMap;
+  return {
+    rows: emptyRows,
+    cols: emptyCols,
+  };
 }
 
 function parseUniverse(input) {
