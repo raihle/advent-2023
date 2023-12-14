3c9fc20ea65f3c7cf11a92c5d0755a834fe8bebd
Jacob Raihle kdm951 <jacob.raihle@teliacompany.com>
2023-12-14 07:25:31 +0200

    Day 13 part 2

--- days/13/index.js
+++ days/13/index.js
@@ -1,30 +1,79 @@
 import { sum } from "../../utils.js";
 
-let stats = {};
 export function run(input) {
   const patterns = parsePatterns(input.trim());
-  console.log("A:", solve(patterns));
+  console.log("A:", a(patterns));
+  console.log("B:", b(patterns));
 }
 
-function solve(patterns) {
+function a(patterns) {
   return patterns.map(findReflection).map(scoreReflection).reduce(sum, 0);
 }
 
-function findReflection(pattern) {
-  const verticalSplit = findVReflection(pattern);
-  const horizontalSplit = findHReflection(pattern);
+function b(patterns) {
+  return patterns
+    .map(findSmudgedReflection)
+    .map(scoreReflection)
+    .reduce(sum, 0);
+}
+
+function allVariations(pattern) {
+  const variations = [];
+  for (let y = 0; y < pattern.length; y++) {
+    const line = pattern[y];
+    for (let x = 0; x < line.length; x++) {
+      const variation = [...pattern];
+      variation[y] =
+        line.substring(0, x) +
+        (line.at(x) == "." ? "#" : ".") +
+        line.substring(x + 1);
+      variations.push(variation);
+    }
+  }
+  return variations;
+}
+
+function findReflection(
+  pattern,
+  reflectionToIgnore = { horizontal: 0, vertical: 0 }
+) {
+  const verticalSplit = findVReflection(pattern, reflectionToIgnore.vertical);
+  const horizontalSplit = findHReflection(
+    pattern,
+    reflectionToIgnore.horizontal
+  );
   return {
     vertical: verticalSplit,
     horizontal: horizontalSplit,
   };
 }
 
-function findHReflection(pattern) {
-  for (
-    let reflectAfter = 1;
-    reflectAfter < pattern.length;
-    reflectAfter++
-  ) {
+function findSmudgedReflection(pattern) {
+  const or = findReflection(pattern);
+
+  const variations = allVariations(pattern);
+  for (const variation of variations) {
+    const vr = findReflection(variation, or);
+    if (vr.vertical == 0 && vr.horizontal == 0) {
+      continue;
+    }
+    if (vr.vertical != or.vertical || vr.horizontal != or.horizontal) {
+      const smudgedReflection = {
+        vertical: or.vertical == vr.vertical ? 0 : vr.vertical,
+        horizontal: or.horizontal == vr.horizontal ? 0 : vr.horizontal,
+      };
+      //console.log("Smudged reflection", smudgedReflection);
+      return smudgedReflection;
+    }
+  }
+  throw new Error(`No smudged reflection found for pattern ${pattern}`);
+}
+
+function findHReflection(pattern, positionToIgnore) {
+  for (let reflectAfter = 1; reflectAfter < pattern.length; reflectAfter++) {
+    if (reflectAfter == positionToIgnore) {
+      continue;
+    }
     let possibleReflection = true;
     const reflectedLength = Math.min(
       reflectAfter,
@@ -58,11 +107,18 @@ function transpose(pattern) {
   return transposed;
 }
 
-function findVReflection(pattern) {
-  return findHReflection(transpose(pattern));
+function findVReflection(pattern, positionToIgnore) {
+  return findHReflection(transpose(pattern), positionToIgnore);
 }
 
 function scoreReflection(reflection) {
+  if (reflection.vertical && reflection.horizontal) {
+    console.log("Double reflection", reflection);
+  } else if (reflection.vertical == 0 && reflection.horizontal == 0) {
+    console.log("Missing reflection");
+  } else {
+    //console.log(reflection);
+  }
   return reflection.vertical + reflection.horizontal * 100;
 }
 
