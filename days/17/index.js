33c03beb5dd0cf8212ac63ff3e5ab52ea4021c03
Jacob Raihle kdm951 <jacob.raihle@teliacompany.com>
2023-12-17 20:35:10 +0200

    Day 17 part 2

--- days/17/index.js
+++ days/17/index.js
@@ -1,10 +1,19 @@
 export function run(input) {
   const map = parseMap(input.trim());
   console.log("A:", a(map));
+  console.log("B:", b(map));
 }
 
 function a(map) {
-  return pathfind(
+  return pathfindA(
+    map,
+    { x: 0, y: 0 },
+    { x: map[0].length - 1, y: map.length - 1 }
+  );
+}
+
+function b(map) {
+  return pathfindB(
     map,
     { x: 0, y: 0 },
     { x: map[0].length - 1, y: map.length - 1 }
@@ -35,6 +44,29 @@ function getTrivialPathCost(map) {
     }
     if (y < map.length - 1) {
       cost += map[y][y + 1];
+    }
+  }
+  return cost;
+}
+
+function getTrivialUltraPathCost(map) {
+  let cost = 0;
+  let x = 0;
+  let y = 0;
+  while (y != map.length - 1 || x != map[0].length - 1) {
+    for (let i = 0; i < 10; i++) {
+      if (x == map[0].length - 1 || (x == map[0].length - 5 && i > 6)) {
+        break;
+      }
+      x++;
+      cost += map[y][x];
+    }
+    for (let i = 0; i < 10; i++) {
+      if (y == map.length - 1 || (y == map.length - 5 && i > 6)) {
+        break;
+      }
+      y++;
+      cost += map[y][x];
     }
   }
   return cost;
@@ -72,7 +104,39 @@ function saw(seen, key, next, bestCost) {
   }
 }
 
-function pathfind(map, start, goal) {
+function sawB(seen, key, next, bestCost) {
+  const from = next.route.at(-2);
+  if (from == undefined) {
+    seen[key].push({
+      cost: next.cost,
+      straightSteps: next.straightSteps,
+      from,
+    });
+    return;
+  }
+  seen[key] = seen[key].filter((s) => s.cost <= bestCost);
+  const existing = seen[key].find(
+    (s) =>
+      (s.from != undefined &&
+        s.from.x == from.x &&
+        s.from.y == from.y &&
+        s.cost >= next.cost &&
+        s.straightSteps == next.straightSteps) ||
+      s.cost >= next.cost + 72
+  );
+  if (existing) {
+    existing.cost = next.cost;
+    existing.straightSteps = next.straightSteps;
+  } else {
+    seen[key].push({
+      cost: next.cost,
+      straightSteps: next.straightSteps,
+      from,
+    });
+  }
+}
+
+function pathfindA(map, start, goal) {
   const trivialPathCost = getTrivialPathCost(map);
   const goals = [];
   let bestCost = trivialPathCost;
@@ -159,12 +223,101 @@ function pathfind(map, start, goal) {
       queue.push(l);
     }
   }
-  return goals.reduce((best, curr) => {
-    if (best.cost < curr.cost) {
-      return best;
+  return bestCost;
+}
+
+function pathfindB(map, start, goal) {
+  const trivialPathCost = getTrivialUltraPathCost(map);
+  let bestCost = trivialPathCost;
+  const goals = [];
+  const seen = {};
+  const queue = [{ at: start, cost: 0, route: [start], straightSteps: 0 }];
+  let handled = 0;
+  while (queue.length > 0) {
+    if (handled % 1_000_000 == 0) {
+      console.log(
+        `Handled ${handled}. In queue: ${queue.length}. Seen: ${
+          Object.entries(seen).length
+        }. Arrivals: ${goals.length}. Best cost: ${bestCost}`
+      );
     }
-    return curr;
-  }).cost;
+    const next = queue.pop();
+    handled++;
+    const key = `${next.at.x},${next.at.y}`;
+    const minRemainingCost =
+      Math.abs(goal.x - next.x) + Math.abs(goal.y - next.y);
+    seen[key] = seen[key] ?? [];
+    if (
+      next.cost + minRemainingCost >= bestCost ||
+      seen[key].some(
+        (s) =>
+          (s.cost <= next.cost &&
+            s.straightSteps == next.straightSteps &&
+            ((s.from == undefined && next.route.length < 2) ||
+              (s.from != undefined &&
+                s.from.x == next.route.at(-2).x &&
+                s.from.y == next.route.at(-2).y))) ||
+          s.cost <= next.cost - 72
+      )
+    ) {
+      continue;
+    }
+    sawB(seen, key, next, bestCost);
+    // TODO Turning limitations
+    if (next.at.x == goal.x && next.at.y == goal.y && next.straightSteps >= 4) {
+      goals.push(next);
+      if (next.cost < bestCost) {
+        bestCost = next.cost;
+      }
+      continue;
+    }
+    let legalSteps = moveInDirections(next.at)
+      .filter(
+        ({ x, y }) => x >= 0 && y >= 0 && x < map[0].length && y < map.length
+      )
+      .map(({ x, y }) => {
+        return {
+          at: { x, y },
+          cost: next.cost + map[y][x],
+          route: [...next.route, { x, y }],
+          straightSteps: 1,
+        };
+      })
+      .filter(({ cost }) => cost < bestCost);
+    if (next.route.length >= 2) {
+      const lastAt = next.route.at(-2);
+      legalSteps = legalSteps.filter(
+        ({ at }) => at.x != lastAt.x || at.y != lastAt.y
+      );
+      const lastDirection = {
+        x: next.at.x - lastAt.x,
+        y: next.at.y - lastAt.y,
+      };
+      const nextStraightStep = {
+        x: next.at.x + lastDirection.x,
+        y: next.at.y + lastDirection.y,
+      };
+      if (next.straightSteps >= 10) {
+        legalSteps = legalSteps.filter(
+          ({ at }) => at.x != nextStraightStep.x || at.y != nextStraightStep.y
+        );
+      } else {
+        const straightStep = legalSteps.find(
+          ({ at }) => at.x == nextStraightStep.x || at.y == nextStraightStep.y
+        );
+        if (straightStep) {
+          straightStep.straightSteps = next.straightSteps + 1;
+        }
+        if (next.straightSteps < 4) {
+          legalSteps = straightStep ? [straightStep] : [];
+        }
+      }
+    }
+    for (const l of legalSteps) {
+      queue.push(l);
+    }
+  }
+  return bestCost;
 }
 
 function parseMap(input) {
