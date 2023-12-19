79c0092ae82006ec98e1d372ef39773a2d6f7d53
Jacob Raihle kdm951 <jacob.raihle@teliacompany.com>
2023-12-19 10:04:59 +0200

    Day 19 part 2

--- days/19/index.js
+++ days/19/index.js
@@ -4,12 +4,122 @@ export function run(input) {
   const { parts, workflows } = parseInput(input.trim());
   console.log(
     "A:",
-    parts.filter((part) => applyWorkflows(workflows, part)).map(addProperties).reduce(sum, 0),
+    parts
+      .filter((part) => applyWorkflows(workflows, part))
+      .map(addProperties)
+      .reduce(sum, 0)
+  );
+  console.log("B:", countAcceptableValues(workflows));
+}
+
+function countAcceptableValues(workflows) {
+  const accepted = findAcceptableValues(workflows, "in");
+  console.log(accepted);
+  return accepted.map(countAcceptableValuesForLimits).reduce(sum, 0);
+}
+
+function countAcceptableValuesForLimits({ x, m, a, s }) {
+  return (
+    (x.max - x.min + 1) *
+    (m.max - m.min + 1) *
+    (a.max - a.min + 1) *
+    (s.max - s.min + 1)
   );
 }
 
+function mergeLimits(la, lb) {
+  const mergedLimits = {};
+  ["x", "m", "a", "s"].forEach((property) => {
+    mergedLimits[property] = {
+      min: Math.max(la[property].min, lb[property].min),
+      max: Math.min(la[property].max, lb[property].max),
+    };
+  });
+  return mergedLimits;
+}
+
+const ACCEPTABLE_VALUES = {
+  A: [
+    {
+      x: { min: 1, max: 4000 },
+      m: { min: 1, max: 4000 },
+      a: { min: 1, max: 4000 },
+      s: { min: 1, max: 4000 },
+    },
+  ],
+  R: [],
+};
+function findAcceptableValues(workflows, workflowName) {
+  if (!ACCEPTABLE_VALUES[workflowName]) {
+    console.log("Calc AV", workflowName);
+    let limits = {
+      x: { min: 1, max: 4000 },
+      m: { min: 1, max: 4000 },
+      a: { min: 1, max: 4000 },
+      s: { min: 1, max: 4000 },
+    };
+    const workflow = workflows[workflowName];
+    let accepted = [];
+    for (const rule of workflow) {
+      const limitsForRule = mergeLimits(limits, passingValues(rule));
+      const acceptedValues = findAcceptableValues(workflows, rule.target).map(
+        (l) => mergeLimits(limitsForRule, l)
+      );
+      accepted = accepted.concat(acceptedValues);
+      limits = mergeLimits(limits, failingValues(rule));
+    }
+    ACCEPTABLE_VALUES[workflowName] = accepted;
+  }
+  return ACCEPTABLE_VALUES[workflowName];
+}
+
+function passingValues(rule) {
+  const limits = {
+    x: { min: 1, max: 4000 },
+    m: { min: 1, max: 4000 },
+    a: { min: 1, max: 4000 },
+    s: { min: 1, max: 4000 },
+  };
+  if (!rule.condition) {
+    return limits;
+  }
+  if (rule.condition.test == ">") {
+    limits[rule.condition.property].min = rule.condition.value + 1;
+  } else if (rule.condition.test == "<") {
+    limits[rule.condition.property].max = rule.condition.value - 1;
+  } else {
+    throw new Error(`Unknown operator ${rule.condition.test}`);
+  }
+  return limits;
+}
+
+function failingValues(rule) {
+  if (!rule.condition) {
+    return {
+      x: { min: 0, max: -1 },
+      m: { min: 0, max: -1 },
+      a: { min: 0, max: -1 },
+      s: { min: 0, max: -1 },
+    };
+  }
+  const limits = {
+    x: { min: 1, max: 4000 },
+    m: { min: 1, max: 4000 },
+    a: { min: 1, max: 4000 },
+    s: { min: 1, max: 4000 },
+  };
+  if (rule.condition.test == ">") {
+    limits[rule.condition.property].max = rule.condition.value;
+  } else if (rule.condition.test == "<") {
+    limits[rule.condition.property].min = rule.condition.value;
+  } else {
+    throw new Error(`Unknown operator ${rule.condition.test}`);
+  }
+  return limits;
+}
+
 function applyWorkflows(workflows, part) {
-  let nextWorkflow = 'in';
+  let nextWorkflow = "in";
   while (nextWorkflow != "R" && nextWorkflow != "A") {
     nextWorkflow = applyWorkflow(workflows[nextWorkflow], part);
   }
@@ -50,9 +160,12 @@ function parseInput(input) {
 
 function parseWorkflows(workflowBlock) {
   const workflows = {};
-  workflowBlock.split("\n").map(parseWorkflow).forEach((wf) => {
-    workflows[wf.name] = wf.rules;
-  });
+  workflowBlock
+    .split("\n")
+    .map(parseWorkflow)
+    .forEach((wf) => {
+      workflows[wf.name] = wf.rules;
+    });
   return workflows;
 }
 
@@ -70,7 +183,7 @@ function parseWorkflow(workflowLine) {
         condition: {
           property: match.groups.property,
           test: match.groups.test,
-          value: match.groups.value,
+          value: parseInt(match.groups.value),
         },
         target: match.groups.target,
       });
